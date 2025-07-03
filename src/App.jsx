2c028e0acc48e86aaa2837f601c30be7eca03871
Arun Yadav <yarun4622@gmail.com>
2025-07-03 23:51:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-[#11111b] xl:p-10 xl:px-52'>
+       <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import img from "../assets/Avatar.jpg"
+
+function Navbar() {
+  return (
+    <div className='w-full h-[10vh]  rounded-full bg-[#112240] border border-white/20'>
+        <div className='w-[5%]'>
+            {/* <img src={img} alt="Arun" /> */}
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
